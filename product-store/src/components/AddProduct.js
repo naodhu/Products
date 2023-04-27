853c2e7c501d0c0c07751248ff757188033d8729
Naod Hunde <chris_naod@yahoo.com>
2023-04-27 19:31:35 +1000

    final commit

--- product-store/src/components/AddProduct.js
+++ product-store/src/components/AddProduct.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import "./ProductForm.css";
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Grid,
+  Paper,
+  Stack,
+} from "@mui/material";
+import { AddShoppingCart } from "@mui/icons-material";
+import { motion } from "framer-motion";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./ProductForm.css";
 
 const AddProduct = () => {
   const history = useNavigate();
@@ -51,82 +61,115 @@ const AddProduct = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="product-form">
-      <TextField
-        value={inputs.title}
-        type="text"
-        label="Title"
-        onChange={handleChange}
-        name="title"
-        required
-      />
-      <TextField
-        type="text"
-        label="Description"
-        name="description"
-        value={inputs.description}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="number"
-        label="Price"
-        name="price"
-        value={inputs.price}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="number"
-        label="Discount Percentage"
-        name="discountPercentage"
-        value={inputs.discountPercentage}
-        onChange={handleChange}
-      />
-      <TextField
-        type="number"
-        label="Rating"
-        name="rating"
-        value={inputs.rating}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="number"
-        label="Stock"
-        name="stock"
-        value={inputs.stock}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="text"
-        label="Brand"
-        name="brand"
-        value={inputs.brand}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="text"
-        label="Category"
-        name="category"
-        value={inputs.category}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        type="text"
-        label="Image URL"
-        name="thumbnail"
-        value={inputs.thumbnail}
-        onChange={handleChange}
-        required
-      />
-      <Button type="submit" variant="contained">
-        Add Product
-      </Button>
-    </form>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Container maxWidth="md" className="product-container">
+        <Paper elevation={5} className="product-paper">
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" className="product-title">
+                Add Product
+              </Typography>
+              <Typography variant="subtitle1" className="product-introduction">
+                Boost your store by adding a new product. Fill in the details
+                below and watch your store grow!
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <form onSubmit={handleSubmit} className="product-form">
+                <TextField
+                  value={inputs.title}
+                  type="text"
+                  label="Title"
+                  onChange={handleChange}
+                  name="title"
+                  required
+                />
+                <TextField
+                  type="text"
+                  label="Description"
+                  name="description"
+                  value={inputs.description}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="number"
+                  label="Price"
+                  name="price"
+                  value={inputs.price}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="number"
+                  label="Discount Percentage"
+                  name="discountPercentage"
+                  value={inputs.discountPercentage}
+                  onChange={handleChange}
+                />
+                <TextField
+                  type="number"
+                  label="Rating"
+                  name="rating"
+                  value={inputs.rating}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="number"
+                  label="Stock"
+                  name="stock"
+                  value={inputs.stock}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="text"
+                  label="Brand"
+                  name="brand"
+                  value={inputs.brand}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="text"
+                  label="Category"
+                  name="category"
+                  value={inputs.category}
+                  onChange={handleChange}
+                  required
+                />
+                <TextField
+                  type="text"
+                  label="Image URL"
+                  name="thumbnail"
+                  value={inputs.thumbnail}
+                  onChange={handleChange}
+                  required
+                />
+                <Stack direction="row" spacing={2}>
+                  <Button type="submit" variant="contained" color="primary">
+                    Add Product
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    startIcon={<AddShoppingCart />}
+                    onClick={() => history("/products")}
+                  >
+                    Back to Products
+                  </Button>
+                </Stack>
+              </form>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </motion.div>
   );
 };
 
--- product-store/src/components/Product/ProductDetail.js
+++ product-store/src/components/Product/ProductDetail.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import { TextField, Button } from "@mui/material";
-import { useState } from "react";
+import {
+  TextField,
+  Button,
+  Container,
+  Typography,
+  Grid,
+  Paper,
+  Stack,
+} from "@mui/material";
+import { Edit } from "@mui/icons-material";
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductDetail = () => {
   const history = useNavigate();
@@ -46,79 +56,111 @@ const ProductDetail = () => {
       [e.target.name]: e.target.value,
     }));
   };
-
   return (
-    <div>
-      {inputs && (
-        <form onSubmit={handleSubmit} className="product-form">
-          <TextField
-            value={inputs.title}
-            type="text"
-            onChange={handleChange}
-            name="title"
-            required
-          />
-          <TextField
-            type="text"
-            name="description"
-            value={inputs.description}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="number"
-            name="price"
-            value={inputs.price}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="number"
-            name="discountPercentage"
-            value={inputs.discountPercentage}
-            onChange={handleChange}
-          />
-          <TextField
-            type="number"
-            name="rating"
-            value={inputs.rating}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="number"
-            name="stock"
-            value={inputs.stock}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="text"
-            name="brand"
-            value={inputs.brand}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="text"
-            name="category"
-            value={inputs.category}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="text"
-            name="thumbnail"
-            value={inputs.thumbnail}
-            onChange={handleChange}
-            required
-          />
-          <Button type="submit" variant="contained">
-            Edit Product
-          </Button>
-        </form>
-      )}
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Container maxWidth="md" className="product-detail-container">
+        <Paper elevation={5} className="product-detail-paper">
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" className="product-detail-title">
+                Edit Product
+              </Typography>
+              <Typography
+                variant="subtitle1"
+                className="product-detail-introduction"
+              >
+                Update the product details to keep your store up to date.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              {inputs && (
+                <form onSubmit={handleSubmit} className="product-detail-form">
+                  <TextField
+                    value={inputs.title}
+                    type="text"
+                    onChange={handleChange}
+                    name="title"
+                    required
+                  />
+                  <TextField
+                    type="text"
+                    name="description"
+                    value={inputs.description}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="number"
+                    name="price"
+                    value={inputs.price}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="number"
+                    name="discountPercentage"
+                    value={inputs.discountPercentage}
+                    onChange={handleChange}
+                  />
+                  <TextField
+                    type="number"
+                    name="rating"
+                    value={inputs.rating}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="number"
+                    name="stock"
+                    value={inputs.stock}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="text"
+                    name="brand"
+                    value={inputs.brand}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="text"
+                    name="category"
+                    value={inputs.category}
+                    onChange={handleChange}
+                    required
+                  />
+                  <TextField
+                    type="text"
+                    name="thumbnail"
+                    value={inputs.thumbnail}
+                    onChange={handleChange}
+                    required
+                  />
+                  <Stack direction="row" spacing={2}>
+                    <Button type="submit" variant="contained" color="primary">
+                      Edit Product
+                    </Button>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      startIcon={<Edit />}
+                      onClick={() => history("/products")}
+                    >
+                      Back to Products
+                    </Button>
+                  </Stack>
+                </form>
+              )}
+            </Grid>
+          </Grid>
+        </Paper>
+      </Container>
+    </motion.div>
   );
 };
 
